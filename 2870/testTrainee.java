e526fb82a3426688f6130fb5f883f743cc4756f4
ragurrk <34347831+ragurrk@users.noreply.github.com>
2017-12-19 21:50:27 -0500

    Add files via upload

---
+++ 2870/testTrainee.java
@@ -0,0 +1,15 @@
+package com.htc.assignments;
+
+public class testTrainee {
+	
+		public static void main(String[] args) {
+	
+			Trainee ent1 = new Trainee();
+			System.out.println(ent1);
+			
+			Trainee ent2 = new Trainee(101, "Ragu", 1234567890, "ragu.test@gmail.com", "Male", 30);
+			System.out.println(ent2);
+
+		}
+	
+}
