2d8d1a5c7910d790090afe44145b231964c38b63
TNRAO5 <34458608+TNRAO5@users.noreply.github.com>
2017-12-20 19:45:53 -0600

    Add files via upload

---
+++ 1580/StringServiceProvider.java
@@ -0,0 +1,29 @@
+package com.htc.week1;
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class StringServiceProvider {
+
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Enter String");
+
+		String mytext = keyboard.nextLine();
+
+		String input = mytext;
+
+		String reverse = new StringBuffer(mytext).reverse().toString()	;
+
+		String rev = reverse.toString();
+
+		System.out.println(rev);
+
+	}
+
+}
