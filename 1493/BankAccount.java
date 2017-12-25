25379181964465f120495d15b5f68334a2bbd10b
sambasrk <34379626+sambasrk@users.noreply.github.com>
2017-12-25 14:31:41 -0600

    Add files via upload

---
+++ 1493/BankAccount.java
@@ -0,0 +1,85 @@
+package com.htc.homework001;
+
+import java.io.Serializable;
+
+public class BankAccount implements Serializable{
+	
+	private String accountNo;
+	private String accountName;
+	private double balance;
+	
+	//override constructor
+	BankAccount(String accountNo,String accountName,double balance){
+		this.accountNo=accountNo;
+		this.accountName=accountName;
+		this.balance=balance=1000;
+	}
+	
+	/**
+	 * @return the accountNo
+	 */
+	public String getAccountNo() {
+		return accountNo;
+	}
+	/**
+	 * @param accountNo the accountNo to set
+	 */
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	/**
+	 * @return the accountName
+	 */
+	public String getAccountName() {
+		return accountName;
+	}
+	/**
+	 * @param accountName the accountName to set
+	 */
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	/**
+	 * @return the balance
+	 */
+	public double getBalance() {
+		return balance;
+	}
+	/**
+	 * @param balance the balance to set
+	 */
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	//hashcode and equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((accountNo == null) ? 0 : accountNo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BankAccount other = (BankAccount) obj;
+		if (accountNo == null) {
+			if (other.accountNo != null)
+				return false;
+		} else if (!accountNo.equals(other.accountNo))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "BankAccount [accountNo=" + accountNo + ", accountName=" + accountName + ", balance=" + balance + "]";
+	}
+	
+	
+
+}
