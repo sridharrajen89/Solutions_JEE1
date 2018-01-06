dc9fe807d54fa7a0bab9ebc815c0eea4f674d03c
UdayaPandian <34431023+UdayaPandian@users.noreply.github.com>
2018-01-05 18:17:54 -0600

    Add files via upload

---
+++ 2810/EXC 10/RegularExpresssion.java
@@ -0,0 +1,66 @@
+package com.htc.Exc2;
+import java.util.regex.*;
+
+public class RegularExpresssion {
+	private String emailID;
+	private String password;
+	private Integer creditCardNo;
+	
+	//Default Constructor
+	public RegularExpresssion() {
+		emailID = "";
+		password = "";
+		creditCardNo = 0;
+	}
+	
+	//Constructor
+	public RegularExpresssion(String emailID, String password, Integer creditCardNo) {
+		super();
+		this.emailID = emailID;
+		this.password = password;
+		this.creditCardNo = creditCardNo;
+	}
+	
+	//Validation methods
+	
+	public boolean emailValidation(String emailID) {
+		return Pattern.matches("[A-Za-z0-9_.]+@[A-Za-z0-9]+\\.[A-Za-z]{2,6}", emailID);
+	}
+	public boolean passwordValidation(String password) {
+		return Pattern.matches("[A-Za-z0-9_.@#$%^&*()-+]{5,8}", password);
+	}
+	public boolean creditCardValidation(Integer creditCard) {
+		return Pattern.matches("[0-9]{10}",String.valueOf(creditCard));
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getCreditCardNo() {
+		return creditCardNo;
+	}
+
+	public void setCreditCardNo(Integer creditCardNo) {
+		this.creditCardNo = creditCardNo;
+	}
+
+	@Override
+	public String toString() {
+		return "RegularExpresssion [emailID=" + emailID + ", password=" + password + ", creditCardNo=" + creditCardNo
+				+ "]";
+	}
+	
+	
+}
