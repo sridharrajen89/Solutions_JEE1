e7eac5ecabf73794e49542a70178f9b817e209c5
UdayaPandian <34431023+UdayaPandian@users.noreply.github.com>
2018-01-05 18:26:05 -0600

    Add files via upload

---
+++ 2810/EXC 3/StringServiceProvider.java
@@ -0,0 +1,44 @@
+package com.htc.Exc2;
+
+public class StringServiceProvider {
+	public static boolean findKeyinString(String str,char key){
+		boolean flag=false;
+		char[] str1=str.toCharArray();
+		for(int i=0;i<str.length();i++){
+			if(key==str1[i]){
+				flag=true;
+				System.out.println("Char "+ key +" Found");
+				break;				
+			}
+		}
+		if(!flag){
+			System.out.println("Char"+ key +"Not found");
+		}
+		return flag;
+	}
+	public static String findKeyandReplacestring(String str,char key, char newkey){
+		boolean flag=false;
+		char[] str1=str.toCharArray();
+		for(int i=0;i<str.length();i++){
+			if(key==str1[i]){
+				str1[i] = newkey;
+			flag=true;
+			}
+		}
+		if(!flag){
+			System.out.println("Not found");
+		}
+		System.out.println("Replace String "+ key +" with " + newkey +" : " +String.valueOf(str1)); 
+		return str; 
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String stringNm = "Udayapandian",stringChg;
+		 stringChg = new StringBuffer(stringNm).reverse().toString();
+		 System.out.println("Given String : "+stringNm);
+		 System.out.println("Reversed String : "+stringChg);
+		 StringServiceProvider.findKeyinString(stringNm, 'U');
+		 StringServiceProvider.findKeyandReplacestring(stringNm, 'n', 'a');		
+	}
+
+}
