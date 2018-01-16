0e4d05f2a3c3fbbf737b81e4caf6a358e56fb36c
Radhika-Jayaraman <34324250+Radhika-Jayaraman@users.noreply.github.com>
2018-01-15 19:33:11 -0600

    Add files via upload

---
+++ 687/XMLEx/Ex6/IPLEx6.java
@@ -0,0 +1,41 @@
+package com.xml.ex;
+
+/**
+ *  Write a program to read and list all the player names of a given team from the IPL XML using SAX. 
+ */
+import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+
+public class IPLEx6 extends DefaultHandler {
+	
+	
+	public void startDocument() {
+		System.out.println("Document Begins here");
+	}
+	
+	public void startElement (String uri, String localName, String qName, Attributes attrs) {
+		System.out.println("<" + qName + "> ");
+	}
+  
+	public void characters (char ch[], int start, int len) {
+		System.out.println(new String (ch, start, len));
+	}
+	
+	public void endElement (String uri, String localName, String qName) {
+		System.out.println("</" + qName + "> ");
+	}	
+	
+	public void endDocument() {
+		System.out.println("Document Ends here");
+	}
+	
+	public static void main (String args []) throws Exception{
+		SAXParser p = SAXParserFactory.newInstance().newSAXParser();
+		p.parse("Players.xml", new MyDefSaxHandler());
+		
+		
+		System.out.println("done");
+	}
+}
